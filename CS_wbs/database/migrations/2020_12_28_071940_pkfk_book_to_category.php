5da99a1a3f82d18bda40434dd9312016df43fcc6
dqthua <duongquangthuan2002@gmail.com>
2020-12-28 14:24:51 +0700

    update pkfk

---
+++ CS_wbs/database/migrations/2020_12_28_071940_pkfk_book_to_category.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PkfkBookToCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+}
